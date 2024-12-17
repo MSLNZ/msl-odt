1d2aef7e6c6992bfe1cc361f68f75f566eb253fb
dunforda <adam.dunford@measurement.govt.nz>
2024-12-18 12:33:06 +1300

    Try to format docstring

--- src/msl/odt/odt.py
+++ src/msl/odt/odt.py
@@ -355,9 +355,9 @@ class Document(object):
         Parameters
         ----------
         text : str
-            Text to add. The use of `\t` and `\n` are supported.
+            Text to add. The use of ``\t`` and ``\n`` are supported.
             Uses ODFPY `teletype` function to handle whitespace.
-            Detects URLs starting with `http://` or `https://`
+            Detects URLs starting with ``http://`` or ``https://``
             and converts them to hyperlinks.
         
         Returns
